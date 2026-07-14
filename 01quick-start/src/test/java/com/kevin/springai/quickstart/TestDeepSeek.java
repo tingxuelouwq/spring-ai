91c9da1a65b12d99ce0012a8ee43c9eb21edc2eb
另一面_where_I_am <kevin_wangqi@126.com>
2026-07-14 22:29:05 +0800

    deepseek思考模式

--- 01quick-start/src/test/java/com/kevin/springai/quickstart/TestDeepSeek.java
+++ 01quick-start/src/test/java/com/kevin/springai/quickstart/TestDeepSeek.java
@@ -3,6 +3,7 @@ package com.kevin.springai.quickstart;
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.deepseek.DeepSeekAssistantMessage;
 import org.springframework.ai.deepseek.DeepSeekChatModel;
 import org.springframework.ai.deepseek.DeepSeekChatOptions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,5 +34,33 @@ public class TestDeepSeek {
         Prompt prompt = new Prompt("请写一句诗描述清晨。", options);
         ChatResponse chatResponse = deepSeekChatModel.call(prompt);
         System.out.println(chatResponse.getResult().getOutput().getText());
+    }
+
+    @Test
+    public void testDeepseekReasoning(@Autowired DeepSeekChatModel deepSeekChatModel) {
+        Prompt prompt =new Prompt("请写一句诗描述今年的股市");
+        ChatResponse response = deepSeekChatModel.call(prompt);
+
+        DeepSeekAssistantMessage assistantMessage = (DeepSeekAssistantMessage) response.getResult().getOutput();
+
+        System.out.println(assistantMessage.getReasoningContent());
+        System.out.println("------------------------------------");
+        System.out.println(assistantMessage.getText());
+    }
+
+    @Test
+    public void testDeepseekStreamReasoning(@Autowired DeepSeekChatModel deepSeekChatModel) {
+        Flux<ChatResponse> stream = deepSeekChatModel.stream(new Prompt("请写一句诗描述今年的招商银行财报"));
+        stream.toIterable().forEach(chatResponse -> {
+            DeepSeekAssistantMessage assistantMessage = (DeepSeekAssistantMessage) chatResponse.getResult().getOutput();
+            System.out.println(assistantMessage.getReasoningContent());
+        });
+
+        System.out.println("-----------------------------------------");
+
+        stream.toIterable().forEach(chatResponse -> {
+            DeepSeekAssistantMessage assistantMessage=  (DeepSeekAssistantMessage)chatResponse.getResult().getOutput();
+            System.out.println(assistantMessage.getText());
+        });
     }
 }
